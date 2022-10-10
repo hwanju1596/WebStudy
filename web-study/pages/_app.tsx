0351ff80a13c8509b50e8805bbf511270f2c9f2b
DESKTOP-LCTSSO7\샵링커2 <hwanju1596@nemo-c.com>
2022-10-10 20:03:17 +0900

    1. CssBaseline 추가.

--- web-study/pages/_app.tsx
+++ web-study/pages/_app.tsx
@@ -1,15 +1,16 @@
 import '../styles/globals.css'
+import * as React from 'react';
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
   );
-  
-}
+};
 
 export default MyApp
